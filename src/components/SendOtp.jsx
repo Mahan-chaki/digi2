c348144eb3439ff27f6115e795941820e1bd4bad
Mahan-chaki <mahanch43@gmail.com>
2024-06-23 17:07:51 +0330

    fdfd

---
+++ src/components/SendOtp.jsx
@@ -0,0 +1,40 @@
+import styles from "./SendOtp.module.css";
+import logo from "../public/logo.svg";
+import toast from "react-hot-toast";
+
+function SendOtp({ setStep, mobile, setMobile }) {
+  const enterHandler = () => {
+    const regex = /09\d{9}/;
+    const result = regex.test(mobile);
+    console.log(result);
+    result ? (
+      setStep(2)
+    ) : (
+      <div className={styles.toast}>
+        {toast.error("شماره موبایل خود را درست وارد کنید")}
+      </div>
+    );
+  };
+
+  return (
+    <div className={styles.container}>
+      <img src={logo} className={styles.img} />
+      <p className={styles.enter}>ورود | ثبت نام</p>
+      <span className={styles.salam}>سلام!</span>
+      <span className={styles.salam2}>لطفا شماره موبایل خود را وارد کنید</span>
+      <input
+        type="text"
+        value={mobile}
+        maxLength={11}
+        onChange={(e) => setMobile(e.target.value)}
+      />
+      <span className={styles.span3}>لطفا این قسمت را خالی نگذارید</span>
+      <button onClick={enterHandler}>ورود</button>
+      <p className={styles.valid}>
+        ورود شما به معنای پذیرش شرایط دیجیکالا و قوانین حریم خصوصی است
+      </p>
+    </div>
+  );
+}
+
+export default SendOtp;
